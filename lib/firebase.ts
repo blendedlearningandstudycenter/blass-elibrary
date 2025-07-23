637a572901754422b2abd47f30e2f4dfc331d757
robotics <robotics@blendedlearningcenter.com>
2025-07-23 09:28:50 +0000

    file uplaod working

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -9,19 +9,11 @@ const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET, // uses .env value
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 }; 
-/* const firebaseConfig = {
-  apiKey: "AIzaSyDbq-j31pjzsLqtvVMZFen5KUKY0iO2t-g",
-  authDomain: "blass-college.firebaseapp.com",
-  projectId: "blass-college",
-  storageBucket: "blass-college.firebasestorage.app",
-  messagingSenderId: "953411325932",
-  appId: "1:953411325932:web:bd0f69b6bc542d7328bac9",
-  measurementId: "G-01MKMJQ7M0"
-}; */
+
 
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
